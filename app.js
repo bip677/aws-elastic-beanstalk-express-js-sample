b4e4810f1753f74b4aa434388d37ecab8a57ff56
unknown <bip677@gmail.com>
2021-04-24 13:37:06 +0530

    second update on local repo

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 8080;
 app.get('/', (req, res) => res.send('Hello , The text displayed after code pipeline manual approval'));
 
 app.listen(port);
-console.log(`App running on http://localhost:${port}`);
+console.log(`App is running on http://localhost:${port}`);
 
 console.log(`The line is added in local repo`);
 
